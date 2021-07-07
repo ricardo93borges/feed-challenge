4e41a43b6e9d404bb2416fa083487b107ec58c2f
Ricardo Borges <ricardo.borges@somosphi.com>
2021-07-07 14:36:14 -0300

    add feed component

--- src/features/feed/index.js
+++ src/features/feed/index.js
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+import { PostCard } from "./components/post-card/PostCard";
+import { getPosts } from "./services/post.service";
+import "./style.css";
+
 export function Feed() {
-  return <></>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const posts = await getPosts();
+      setPosts(posts);
+    }
+
+    getData();
+  }, []);
+
+  return (
+    <div className="feed">
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
