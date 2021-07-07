3a4a2ccffc23130e6977c8524c60e2834a13e1c5
Ricardo Borges <ricardo.borges@somosphi.com>
2021-07-07 15:48:29 -0300

    stores fetched post in redux state

--- src/features/feed/index.js
+++ src/features/feed/index.js
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { PostCard } from "./components/post-card/PostCard";
+import { set } from "./reducers/posts.reducer";
 import { getPosts } from "./services/post.service";
 import "./style.css";
 
 export function Feed() {
-  const [posts, setPosts] = useState([]);
+  const dispatch = useDispatch();
+  const localPosts = useSelector(({ posts }) => posts.posts);
+
+  const [posts, setPosts] = useState(localPosts);
 
   useEffect(() => {
     async function getData() {
       const posts = await getPosts();
       setPosts(posts);
+      dispatch(set(posts));
     }
 
-    getData();
+    if (posts.length === 0) {
+      getData();
+    }
   }, []);
 
   return (
