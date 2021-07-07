fb9e3afed8cf6bcbc3847f5d770bd526fb6b0af3
Ricardo Borges <ricardo.borges@somosphi.com>
2021-07-07 14:49:27 -0300

    add post component

--- src/features/feed/components/post/Post.js
+++ src/features/feed/components/post/Post.js
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getPostById } from "../../services/post.service";
+import "./Post.css";
+
 export function Post() {
-  return <></>;
+  let { id } = useParams();
+
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const posts = await getPostById(id);
+      setPost(posts);
+    }
+
+    getData();
+  }, []);
+
+  return (
+    <div className="post">
+      {!post && <p>post not found</p>}
+      {post && (
+        <div>
+          <h2>{post.title}</h2>
+          <article>{post.body}</article>
+        </div>
+      )}
+      <h3>Comments</h3>
+    </div>
+  );
 }
