8eb4c9b66a25bb3bd0fe255d9860c396cb06f43b
Ricardo Borges <ricardo.borges@somosphi.com>
2021-07-07 15:15:07 -0300

    update store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import commentsReducer from "../features/feed/reducers/comments.reducer";
+import postsReducer from "../features/feed/reducers/posts.reducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    posts: postsReducer,
+    comments: commentsReducer,
+  },
 });
